77b97fbd6a2db0df8622f35879df10728dd63026
dayofthetech <j.alberto.reyes.r90@gmail.com>
2023-08-02 20:43:00 -0400

    MUI framework installed and skeleton form create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { getAllData } from './util/index';
+import Contact from './components/Contact';
 
 const URL = 'http://localhost:8000/api/v1/';
 
 function App() {
 
-const [message, setMessage] = useState(''); 
+const [message, setMessage] = useState('');
 
   useEffect(() => {
 
@@ -13,7 +14,7 @@ const [message, setMessage] = useState('');
       const myData = await getAllData(URL)
       setMessage(myData.data);
     })();
-      
+
     return () => {
       console.log('unmounting');
     }
@@ -23,6 +24,7 @@ const [message, setMessage] = useState('');
   return (
     <>
       <h1>{message}</h1>
+      <Contact />
     </>
   );
 }

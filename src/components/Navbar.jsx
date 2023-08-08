04f78a879347f1f8bcd72029e2c2c52a9ae2c298
dayofthetech <j.alberto.reyes.r90@gmail.com>
2023-08-07 20:21:55 -0400

    testing github action -

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import { AppBar, Menu, Toolbar, Typography } from '@mui/material';
-
+import React, { useState } from 'react';
+import { AppBar, Button, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
 
 const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <React.Fragment>
-        <AppBar>
-          <Toolbar>
-          <Typography>
-                MENU
-            </Typography>
-          </Toolbar>
-        </AppBar>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6">MUSIC APP</Typography>
+          <Button onClick={handleMenuOpen} color="inherit">
+            MENU
+          </Button>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose}>Account</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Sign In</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Sign Up</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

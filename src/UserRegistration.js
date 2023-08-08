8996a8a8d907c22c1c836e4b308cfd9fa1d3c59f
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-08-08 12:23:41 -0400

    use react-input library to create a credit card input component and submit information from form

--- src/UserRegistration.js
+++ src/UserRegistration.js
@@ -2,22 +2,40 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CreditCardInput from './components/CreditCardInput';
 
 function UserRegistration() {
   const defaultTheme = createTheme();
+  const handle = (e) => {
+    e.preventDefault();
+    console.log('The form was submitted');
+    console.dir(e);
+    const form = e.target;
+    const email = form['email'].value;
+    const cardNumber = form['cardNumber'].value;
+    const expiryDate = form['expiryDate'].value;
+    const cvc = form['cvc'].value;
+    console.log(
+      `Got form fields: ${email}, ${cardNumber}, ${expiryDate}, ${cvc}`
+    );
+  };
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
+        <form onSubmit={handle}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <CreditCardInput />
+          <button type="submit">Submit</button>
+        </form>
       </Container>
     </ThemeProvider>
   );
---
+++ src/components/CreditCardInput.js
@@ -0,0 +1,21 @@
+import { PaymentInputsWrapper, usePaymentInputs } from 'react-payment-inputs';
+import images from 'react-payment-inputs/images';
+
+export default function CreditCardInput() {
+  const {
+    wrapperProps,
+    getCardImageProps,
+    getCardNumberProps,
+    getExpiryDateProps,
+    getCVCProps,
+  } = usePaymentInputs();
+
+  return (
+    <PaymentInputsWrapper {...wrapperProps}>
+      <svg {...getCardImageProps({ images })} />
+      <input {...getCardNumberProps()} />
+      <input {...getExpiryDateProps()} />
+      <input {...getCVCProps()} />
+    </PaymentInputsWrapper>
+  );
+}

2eabacc61b32f34039ff75326f171a80739eb7de
Masooma <masuma.jafari@gmail.com>
2023-08-15 11:51:35 -0700

    pass the first test in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
+import { startMirage } from './util/mirageServer';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 let server;
 beforeEach(() => {
@@ -9,8 +11,12 @@ afterEach(() => {
   server.shutdown();
 });
 
-test('render home page', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/This is a music app/i);
+test('render home page', async () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = await screen.findByText(/This is a music app/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import { useIsAuthenticated } from 'react-auth-kit';
 
 function Home() {
-  const isAuthenticated = useIsAuthenticated();
-
-  if (isAuthenticated()) {
-    console.log('authenticated');
-  } else {
-    console.log('not authenticated');
-  }
   return <h1>THIS IS THE HOME PAGE</h1>;
 }
 

adc56c056d947f0dca2d5840a854fdce1dcaf073
Raheim Grant <rgran065@fiu.edu>
2023-08-15 15:18:49 -0400

    Viewmyprofile

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+ 
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import ViewUserProfile from './components/UserProfile'; // Import the ViewUserProfile component
+
+
+const mockUser = {
+  username: john123,
+  name: 'John Doe',
+  email: 'johndoe@example.com',
+  bio: 'The song that I am loving this week is Munekita.',
+  roles: [
+    { id: 1, name: 'User' },
+    { id: 2, name: 'Admin' },
+  ],
+  image:
+    'https://cdn.dribbble.com/users/6142/screenshots/5679189/media/1b96ad1f07feee81fa83c877a1e350ce.png?compress=1&resize=400x300&vertical=top',
+};
+
+const handleUpdateProfileClick = () => {
+  // Handle profile update
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

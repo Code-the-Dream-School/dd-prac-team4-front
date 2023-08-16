c7e31548466db0e611552af7f8a0ee6a727e3ccc
Masooma <masuma.jafari@gmail.com>
2023-08-15 22:33:18 -0700

    create a Navbar component and edit logout button style

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import UserRegistration from './components/UserRegistration';
 import SignIn from './components/SignIn';
 import Home from './components/Home';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Logout from './components/Logout';
+import Navbar from './components/Navbar';
 import {
   AuthStatus,
   RequireAuth,
@@ -31,6 +33,7 @@ function App() {
 
   return (
     <>
+      <Navbar />
       <h1>{message}</h1>
       <Routes>
         <Route
@@ -40,6 +43,7 @@ function App() {
         <Route path="/register" element={<UserRegistration />}></Route>
         <Route path="/signIn" element={<SignIn />}></Route>
         <Route path="/home" element={<Home />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
       </Routes>
     </>
   );

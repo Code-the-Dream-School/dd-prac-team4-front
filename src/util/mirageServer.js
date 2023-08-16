8345416fe03d2de4b01b8b9486d4b7f2e1f4a852
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-08-16 12:45:54 -0400

    replace mirage fix

--- src/util/mirageServer.js
+++ src/util/mirageServer.js
@@ -3,6 +3,17 @@ import { createServer } from 'miragejs';
 export function startMirage() {
   return createServer({
     routes() {
+      // FIX bug with how mirage + axiox interact
+      // https://github.com/miragejs/miragejs/issues/814 -->
+      const NativeXMLHttpRequest = window.XMLHttpRequest;
+
+      window.XMLHttpRequest = function XMLHttpRequest() {
+        const request = new NativeXMLHttpRequest(arguments);
+        delete request.onloadend;
+        return request;
+      };
+      // <-- FIX
+
       this.get('http://localhost:8000/api/v1', { data: 'This is a music app' }),
         // route to submit user registration form
         this.post(

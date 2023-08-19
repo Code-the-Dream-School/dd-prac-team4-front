8b256b36175a13367509d15a157b1c8de5d15124
dayofthetech <j.alberto.reyes.r90@gmail.com>
2023-08-19 16:12:48 -0400

    moving to main

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const SearchList = () => {
   const [results, setResults] = useState([]);
 
   const fetchResults = async () => {
+    try {
     // Fetching from backend API
-    const response = await fetch('http://localhost:8000/api/v1');
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await response.json();
+    // loging results
+    console.log("Fetched data:", data); // Log the fetched data
     setResults(data);
-  };
+  } catch(error) {
+    console.error("Error fetching", error);
+  }
+};
 
   useEffect(() => {
     fetchResults();
@@ -21,6 +27,7 @@ const SearchList = () => {
       ))}
     </ul>
   );
+
 };
 
 export default SearchList;

1701998246a0f490b85a988522bf786a1ab1820a
dayofthetech <j.alberto.reyes.r90@gmail.com>
2023-08-20 17:28:36 -0400

    displaying albums img - component structure req

--- src/App.js
+++ src/App.js
@@ -1,67 +1,44 @@
-// import React, { useState, useEffect } from 'react';
-// import { getAllData } from './util/index';
-// import { createServer } from 'miragejs';
-// // import SignIn from './components/SignIn';
-
-// createServer({
-//   routes() {
-//     // FIX bug with how mirage + axiox interact
-//     // https://github.com/miragejs/miragejs/issues/814 -->
-//     const NativeXMLHttpRequest = window.XMLHttpRequest;
-
-//     window.XMLHttpRequest = function XMLHttpRequest() {
-//       const request = new NativeXMLHttpRequest(arguments);
-//       delete request.onloadend;
-//       return request;
-//     };
-//     // <-- FIX
-
-//     this.get('http://localhost:8000/api/v1', { data: 'This is a music app' }),
-//       this.passthrough('http://localhost:8000/*'); // everything else will try to actually call the backend
-//   },
-// });
-
-// const URL = 'http://localhost:8000/api/v1/';
-
-// function App() {
-//   const [message, setMessage] = useState('');
-
-//   useEffect(() => {
-//     (async () => {
-//       const myData = await getAllData(URL);
-//       setMessage(myData.data);
-//     })();
-
-//     return () => {
-//       console.log('unmounting');
-//     };
-//   }, []);
-
-//   return (
-//     <>
-//       <h1>{message}</h1>
-//       <h1>learn react</h1>
-//       {/* <SignIn /> */}
-//     </>
-//   );
-// }
-
-// export default App;
-
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import SearchList from './components/SearchList' // Import the SearchList component
+import { getAllData } from './util/index';
+import { createServer } from 'miragejs';
+import AlbumGrid from './components/AlbumGrid';
 import './App.css';
+import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/v1/albums';
+createServer({
+  routes() {
+    // FIX bug with how mirage + axiox interact
+    // https://github.com/miragejs/miragejs/issues/814 -->
+    const NativeXMLHttpRequest = window.XMLHttpRequest;
+
+    window.XMLHttpRequest = function XMLHttpRequest() {
+      const request = new NativeXMLHttpRequest(arguments);
+      delete request.onloadend;
+      return request;
+    };
+    // <-- FIX
+
+    this.get('http://localhost:8000/api/v1', { data: 'This is a music app' }),
+      this.passthrough('http://localhost:8000/*'); // everything else will try to actually call the backend
+  },
+});
+
+const URL = 'http://localhost:8000/api/v1/';
 
 function App() {
+  const [message, setMessage] = useState('');
   const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
+    (async () => {
+      const myData = await getAllData(URL);
+      setMessage(myData.data);
+    })();
+
+    // Fetch albums data
     async function fetchAlbums() {
       try {
-        const response = await axios.get(API_URL);
+        const response = await axios.get(URL + 'albums');
         setAlbums(response.data.albums.slice(0, 10));
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -69,15 +46,23 @@ function App() {
     }
 
     fetchAlbums();
+
+    return () => {
+      console.log('unmounting');
+    };
   }, []);
 
   return (
-    <div className="album-grid">
+    <>
+      <h1>{message}</h1>
+      <h1>learn react</h1>
+      {/* <SignIn /> */}
+
       <h1>Top 10 Albums</h1>
-      <SearchList albums={albums} /> {/* Use the SearchList component */}
-    </div>
+      <AlbumGrid albums={albums} /> {/* Use the AlbumGrid component */}
+
+    </>
   );
 }
 
 export default App;
-

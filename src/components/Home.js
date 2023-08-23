1674448ff986cc72a080d9714f7b663f10f13e6f
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-08-23 13:34:37 -0700

    Loader component removed from Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,6 @@ function Home() {
       <h1>THIS IS THE HOME PAGE</h1>
       <p>Current log-in status is: {status}</p>
       <p>Current logged-in user is: {JSON.stringify(user)}</p>
-      <Loader className="small-spinner" />
     </div>
   );
 }

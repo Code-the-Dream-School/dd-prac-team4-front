5708cdf65a499d34b5fd841b69e5d15fcc660008
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-08-23 09:05:19 -0700

    Loader implenented

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import { useAuth } from '@akosasante/react-auth-context';
+import Loader from '../components/layout/Loader/Loader';
 
 function Home() {
   const { status, user } = useAuth();
@@ -8,6 +9,7 @@ function Home() {
       <h1>THIS IS THE HOME PAGE</h1>
       <p>Current log-in status is: {status}</p>
       <p>Current logged-in user is: {JSON.stringify(user)}</p>
+      <Loader className="small-spinner" />
     </div>
   );
 }

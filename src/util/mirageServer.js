e633d66139b81953665f56322281fa7a99453f6c
Masooma <masuma.jafari@gmail.com>
2023-08-23 13:55:41 -0700

    update mirageServer.js file

--- src/util/mirageServer.js
+++ src/util/mirageServer.js
@@ -15,43 +15,14 @@ export function makeServer() {
       // <-- FIX
 
       this.get('http://localhost:8000/api/v1', { data: 'This is a music app' }),
-        // route to submit user registration form
-        this.post(
-          'http://localhost:8000/api/v1/auth/register',
+        // route to get current user
+        this.get(
+          'http://localhost:8000/api/v1/auth/users/showMe',
           (schema, request) => {
-            console.dir(schema);
-            console.dir('request' + JSON.parse(request));
             let attrs = JSON.parse(request.requestBody);
             console.log(attrs);
-            const loggedInUser = {
-              name: attrs.name,
-            };
-            return { user: loggedInUser }; //return name of logged in user to home page
           }
         );
-      //route to logout user
-      this.post(
-        'http://localhost:8000/api/v1/auth/logout',
-        (schema, request) => {
-          let attrs = JSON.parse(request.requestBody);
-          console.log(attrs);
-        }
-      );
-      // route to get current user
-      this.get(
-        'http://localhost:8000/api/v1/auth/showMe',
-        (schema, request) => {
-          let attrs = JSON.parse(request.requestBody);
-          console.log(attrs);
-        }
-      );
-      // this.get(
-      //   'http://localhost:8000/api/v1/albums/filter',
-      //   (schema, request) => {
-      //     let attrs = JSON.parse(request.requestBody);
-      //     console.log(attrs);
-      //   }
-      // );
 
       this.passthrough('http://localhost:8000/*'); // everything else will try to actually call the backend
     },

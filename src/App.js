d2d7a6838dd986969d73a2413379cf231f6f2503
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-08-24 19:00:07 -0400

    with loading and error state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { AuthStatus, useAuth } from '@akosasante/react-auth-context';
 import usersApi from './apis/usersApi';
+import Loader from './components/layout/Loader/Loader';
 
 const URL = 'http://localhost:8000/api/v1/';
 
@@ -14,21 +15,29 @@ function App() {
   const [message, setMessage] = useState('');
   const { status } = useAuth();
   const isLoggedIn = status === AuthStatus.LoggedIn;
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    // define an async function and call it immediately (by adding the parentheses at the end)
+    // same as doing : const fetchData = async () => { ... }; fetchData();
     (async () => {
-      const user = await usersApi.getById('64e75e0c010653c8fae9a395');
-      setMessage(`HELLO: ${user.name}`);
+      try {
+        setLoading(true);
+        const user = await usersApi.getById('64e75e0c010653c8fae9a391');
+        setLoading(false);
+        setMessage(`HELLO: ${user.name}`);
+      } catch (error) {
+        setMessage('error loading data: ' + error.message);
+      }
     })();
-    return () => {
-      console.log('unmounting');
-    };
+
+    // empty array in the second argument means that this effect will only run once (like componentDidMount in class components)
   }, []);
 
   return (
     <>
       <Navbar />
-      <h1>{message}</h1>
+      {loading ? <Loader className="small-spinner" /> : <h1>{message}</h1>}
       <Routes>
         <Route
           path="/"

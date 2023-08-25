8ab15bdea8b77dc29ed94e0ab910803c3cb5e500
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-08-24 21:19:28 -0700

    orderInfo added

--- src/components/Purchase/CheckoutPage.js
+++ src/components/Purchase/CheckoutPage.js
@@ -10,7 +10,13 @@ import CheckoutForm from './CheckoutForm';
 const stripePromise = loadStripe(
   'pk_test_51Nfj2pKnb0YvaiB3W0z31bbB7OPEnqem6wnpSbohudeiDkj1NUde9M5kgUwozzrfBqGqRpWU5ivDIInzWdt4q5zw00vMz7fc1c'
 );
-const paymentAmount = 1099; // This is $10.99 payment amount(added for testing) This amount variable should came from cart
+//Temporary variables for testing
+//const paymentAmount = 1099; // This is $10.99 payment amount(added for testing) This amount variable should came from cart
+const album = '876r75675';
+const quantity = 1;
+const subtotal = 100.0;
+const tax = 0.4;
+const total = 140.0;
 
 const CheckoutPage = () => {
   const [clientSecret, setClientSecret] = useState('');
@@ -21,11 +27,16 @@ const CheckoutPage = () => {
       credentials: 'include',
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ paymentAmount }),
+      body: JSON.stringify({
+        orderItems: [{ album, quantity }],
+        subtotal,
+        tax,
+        total,
+      }),
     })
       .then((res) => res.json())
       .then((data) => setClientSecret(data.clientSecret));
-  }, [paymentAmount]);
+  }, [subtotal]);
 
   const appearance = {
     theme: 'stripe',
@@ -41,7 +52,7 @@ const CheckoutPage = () => {
         {clientSecret && (
           <>
             <Elements options={options} stripe={stripePromise}>
-              <CheckoutForm paymentAmount={paymentAmount} />
+              <CheckoutForm paymentAmount={subtotal} />
             </Elements>
           </>
         )}

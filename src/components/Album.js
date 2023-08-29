2f3a96d99c402268597be1988303d120bf542ca3
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-08-29 11:59:26 -0700

    Run npx prettier . --write

--- src/components/Album.js
+++ src/components/Album.js
@@ -16,6 +16,7 @@ function Album({ album }) {
           </Button> */}
           {/*AddToCartButton component */}
           <AddToCartButton album={album} />
+
           <Button
             variant="contained"
             color="secondary"

ce1d934c02831baa65f1a47b347501ba633db6af
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-08-29 11:52:15 -0700

    Removed temporarily created for testing component ShoppingCartComponents

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { AuthStatus, useAuth } from '@akosasante/react-auth-context';
 import CheckoutPage from './components/Purchase/CheckoutPage';
-import ShoppingCart from './components/ShoppingCartComponents';
 
 const URL = 'http://localhost:8000/api/v1/';
 
@@ -39,7 +38,6 @@ function App() {
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/home" element={<Home />} />
         <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/shoppingCart" element={<ShoppingCart />} />
       </Routes>
     </>
   );

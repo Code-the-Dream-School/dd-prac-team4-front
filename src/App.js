8afde1fe3c1fc0d10f97bd644bc15efa244fc462
dayofthetech <j.alberto.reyes.r90@gmail.com>
2023-08-28 20:35:05 -0400

    ticket PS-55 album details

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { AuthStatus, useAuth } from '@akosasante/react-auth-context';
 import CheckoutPage from './components/Purchase/CheckoutPage';
+import AlbumDetails from './components/AlbumDetails';
 
 function App() {
   const { status } = useAuth();
@@ -22,9 +23,31 @@ function App() {
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/home" element={<Home />} />
         <Route path="/checkout" element={<CheckoutPage />} />
+
+        {/* Ticket PS-55 */}
+        {/* <Route path="/albums" element={<AlbumDetails />} /> */}
+        {/* <AlbumDetails/> */}
       </Routes>
     </>
   );
 }
 
 export default App;
+
+//--- barebones code --
+// use this to display record details
+
+// import React from 'react';
+// import AlbumDetails from './components/AlbumDetails';
+
+// const App = () => {
+//   return (
+//     <div>
+//       {/* Other components */}
+//       <AlbumDetails />
+//     </div>
+//   );
+// };
+
+// export default App;
+

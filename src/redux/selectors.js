c56ebf41155a96198d6f75053e11d44f7328ba63
Masooma <masuma.jafari@gmail.com>
2023-08-30 14:22:41 -0700

    update orderSidebar file

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,5 @@
 const selectAlbumsInCart = (state) => {
-  const cartValues = Object.values(state.cart.cartItems); // returns an array with the value of each entry in the cartItems object
+  const cartValues = Object.values(state.cart.items); // returns an array with the value of each entry in the cartItems object
   const albums = cartValues.map((cartVal) => cartVal.album);
   return albums;
 };

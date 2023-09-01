d180dad875c3046349390a9e0c9019a06244a422
Masooma <masuma.jafari@gmail.com>
2023-09-01 01:31:40 -0700

    remove test from APP.JS

--- src/redux/selectors.js
+++
@@ -1,7 +0,0 @@
-const selectAlbumsInCart = (state) => {
-  const cartValues = Object.values(state.cart.items); // returns an array with the value of each entry in the cartItems object
-  const albums = cartValues.map((cartVal) => cartVal.album);
-  return albums;
-};
-
-export { selectAlbumsInCart };

c297e67547a7caf29137e3453047f298e3489221
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-09-02 22:28:04 -0700

    Changes are made according to the mentor's recommendations

--- src/components/layout/AddToWishlistButton/AddToWishlistButton.js
+++ src/components/layout/AddToWishlistButton/AddToWishlistButton.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
-import axios from 'axios';
+import axiosInstance from '../../../apis/axiosClient'; // axios instance
 import { AuthStatus, useAuth } from '@akosasante/react-auth-context';
 import { useNavigate } from 'react-router-dom';
+
+const apiBaseURL = process.env.REACT_APP_API_BASE_PATH;
+const wishlistEndpoint = `${apiBaseURL}/wishlist`;
 
 const AddToWishlistButton = ({ album }) => {
   const [isAdded, setIsAdded] = useState(false);
@@ -12,13 +15,7 @@ const AddToWishlistButton = ({ album }) => {
 
   const fetchOrCreateWishlist = async () => {
     try {
-      const response = await axios.post(
-        'http://localhost:8000/api/v1/wishlist',
-        { albumId: album._id },
-        {
-          withCredentials: true,
-        }
-      );
+      const response = await axiosInstance.post(wishlistEndpoint);
       return response.data.wishlist._id; // Return the wishlist ID
     } catch (error) {
       console.error('Error creating wishlist:', error);
@@ -34,12 +31,8 @@ const AddToWishlistButton = ({ album }) => {
   const addToWishlist = async () => {
     const wishlistId = await fetchOrCreateWishlist(); // Fetch the wishlist ID
     try {
-      await axios.patch(
-        `http://localhost:8000/api/v1/wishlist/${wishlistId}/add_album/${album._id}`,
-        null,
-        {
-          withCredentials: true,
-        }
+      await axiosInstance.patch(
+        `${wishlistEndpoint}/${wishlistId}/add_album/${album._id}`
       );
       localStorage.setItem(`addedStatus-${album._id}`, JSON.stringify(true));
       setIsAdded(true);
@@ -51,12 +44,8 @@ const AddToWishlistButton = ({ album }) => {
   const removeFromWishlist = async () => {
     const wishlistId = await fetchOrCreateWishlist(); // Fetch the wishlist ID
     try {
-      await axios.patch(
-        `http://localhost:8000/api/v1/wishlist/${wishlistId}/remove_album/${album._id}`,
-        null,
-        {
-          withCredentials: true,
-        }
+      await axiosInstance.patch(
+        `${wishlistEndpoint}/${wishlistId}/remove_album/${album._id}`
       );
       localStorage.setItem(`addedStatus-${album._id}`, JSON.stringify(false));
       setIsAdded(false);

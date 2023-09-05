c8a7e86a17add60c397cea88a85c81529a6e360d
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-09-04 23:12:23 -0700

    Subcomponent CheckoutMessageCard is implemented according to the mentor's recommendations. v.1.1

--- src/components/Purchase/CheckoutComplete.js
+++ src/components/Purchase/CheckoutComplete.js
@@ -1,18 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Elements } from '@stripe/react-stripe-js';
 import { Container, Paper, Typography, Grid, Box } from '@mui/material';
-import NavigateButton from '../layout/NavigateButton/NavigateButton';
 import style from './CheckoutComplete.module.css';
-import PaymentStatus from './PaymentStatus';
-import { loadStripe } from '@stripe/stripe-js';
 import { useSearchParams } from 'react-router-dom';
 import Loader from '../layout/Loader/Loader';
 import axiosInstance from '../../apis/axiosClient';
-import { useAuth } from '@akosasante/react-auth-context';
-
-const stripePromise = loadStripe(
-  'pk_test_51Nfj2pKnb0YvaiB3W0z31bbB7OPEnqem6wnpSbohudeiDkj1NUde9M5kgUwozzrfBqGqRpWU5ivDIInzWdt4q5zw00vMz7fc1c'
-);
+import CheckoutMessageCard from './CheckoutMessageCard';
 
 const ordersEndpoint = '/orders';
 
@@ -21,13 +13,10 @@ const CheckoutComplete = () => {
   const clientSecret = searchParams.get('payment_intent_client_secret');
   const orderId = searchParams.get('orderId');
 
-  const [isPaymentSuccessful, setIsPaymentSuccessful] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
-  const [orderData, setOrderData] = useState(null);
-
-  const { user } = useAuth();
-  const userEmail = user.user.email;
+  const [orderData, setOrderData] = useState('');
+  const [isPaymentSuccessful, setIsPaymentSuccessful] = useState(false);
 
   // Callback function to update isPaymentSuccessful
   const updatePaymentStatus = (isSuccess) => {
@@ -42,83 +31,67 @@ const CheckoutComplete = () => {
         const response = await axiosInstance.get(`${ordersEndpoint}/${id}`);
         setIsLoading(false);
         setOrderData(response.data?.order);
-        console.log(JSON.stringify(response.data?.order));
+        console.log(response.data?.order);
       } catch (error) {
         setIsLoading(false);
         setErrorMessage(
-          `An error occurred during loading order details with id ${id}`
+          `An error occurred during loading order details with id ${id}. ${error.message}`
         );
       }
     };
     orderDetails(orderId);
   }, [clientSecret]);
 
+  // Calculate the total number of items purchased
+  let totalItemsPurchased = 0;
+  if (orderData && orderData.orderItems) {
+    orderData.orderItems.forEach((item) => {
+      totalItemsPurchased += item.quantity;
+    });
+  }
+
   return (
     <>
       <Container className={style.mainContainer}>
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6}>
-            <Paper className={`${style.paper} ${style.leftPaper}`}>
-              <Box mt="1rem">
-                <Typography variant="h4">
-                  <Elements stripe={stripePromise} options={{ clientSecret }}>
-                    <PaymentStatus
-                      clientSecret={clientSecret}
-                      updateStatus={updatePaymentStatus}
-                    />
-                  </Elements>
-                </Typography>
-              </Box>
-              {isPaymentSuccessful ? (
-                <>
-                  {isLoading ? (
-                    <Loader className="small-spinner" />
-                  ) : errorMessage !== '' ? (
-                    <>
-                      <Box mt="2rem">
-                        <Typography variant="h8">
-                          {`Your order of ${1} albums has been completed.`}
-                          <br />
-                          {`A confirmation email was sent to ${userEmail}`}
-                        </Typography>
-                      </Box>
-                      <Box mt="2rem">
-                        <NavigateButton
-                          linkName={'/'}
-                          variant={'contained'}
-                          color={'secondary'}
-                          children={'Return to Marketplace'}
-                          fullWidth
-                        />
-                      </Box>
-                    </>
-                  ) : (
-                    <Typography variant="h6">{errorMessage}</Typography>
-                  )}
-                </>
-              ) : null}
-            </Paper>
+            <CheckoutMessageCard
+              isLoading={isLoading}
+              clientSecret={clientSecret}
+              totalItemsPurchased={totalItemsPurchased}
+              updateStatus={updatePaymentStatus}
+            />
           </Grid>
 
           <Grid item xs={12} sm={6}>
             <Paper className={`${style.paper} ${style.rightPaper}`}>
-              {isPaymentSuccessful ? (
+              {isLoading ? (
+                <Loader className="small-spinner" />
+              ) : errorMessage !== '' ? (
+                <Typography variant="h6">{errorMessage}</Typography>
+              ) : isPaymentSuccessful ? (
                 <>
-                  {isLoading ? (
-                    <Loader className="small-spinner" />
-                  ) : errorMessage !== '' ? (
-                    <>
-                      <Typography variant="h6">Order Details:</Typography>
-                      {/* --------- */}
-                      <Typography variant="h6">
-                        Order ID: {orderData._id}
-                      </Typography>
-                      <Typography>Amount: ${orderData.total}</Typography>
-                      {/* ---------- */}
-                    </>
-                  ) : (
-                    <Typography variant="h6">{errorMessage}</Typography> // Other order data fields can be added here
-                  )}
+                  <Box mt="2rem">
+                    <Typography variant="h4">Order Details:</Typography>
+                  </Box>
+                  <Box mt="2rem">
+                    <Typography variant="h6">
+                      Order ID: {orderData._id}
+                    </Typography>
+                    {orderData.orderItems.map((item, index) => (
+                      <div key={index}>
+                        <Typography>Album ID: {item.album}</Typography>
+                        <Typography>Quantity: {item.quantity}</Typography>
+                      </div>
+                    ))}
+                  </Box>
+                  <Box mt="2rem">
+                    <Typography>Subtotal: ${orderData.subtotal}</Typography>
+                    <Typography>Tax: {orderData.tax}</Typography>
+                    <Typography variant="h6">
+                      Total: ${orderData.total}
+                    </Typography>
+                  </Box>
                 </>
               ) : (
                 <Typography variant="h6">
---
+++ src/components/Purchase/CheckoutMessageCard.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import { Paper, Typography, Box } from '@mui/material';
+import { loadStripe } from '@stripe/stripe-js';
+import Loader from '../layout/Loader/Loader';
+import { Elements } from '@stripe/react-stripe-js';
+import NavigateButton from '../layout/NavigateButton/NavigateButton';
+import PaymentStatus from './PaymentStatus';
+import { useAuth } from '@akosasante/react-auth-context';
+import style from './CheckoutMessageCard.module.css';
+
+const stripePromise = loadStripe(
+  'pk_test_51Nfj2pKnb0YvaiB3W0z31bbB7OPEnqem6wnpSbohudeiDkj1NUde9M5kgUwozzrfBqGqRpWU5ivDIInzWdt4q5zw00vMz7fc1c'
+);
+
+const CheckoutMessageCard = ({
+  isLoading = { isLoading },
+  clientSecret,
+  totalItemsPurchased,
+  updateStatus,
+}) => {
+  const { user } = useAuth();
+  const userEmail = user.user.email;
+  const [isPaymentSuccessful, setIsPaymentSuccessful] = useState(false);
+
+  // Callback function to update isPaymentSuccessful
+  const updatePaymentStatus = (isSuccess) => {
+    setIsPaymentSuccessful(isSuccess);
+  };
+
+  useEffect(() => {
+    updateStatus(isPaymentSuccessful);
+  }, [updateStatus, isPaymentSuccessful]);
+
+  return (
+    <>
+      <Paper className={`${style.paper} ${style.leftPaper}`}>
+        <Box mt="1rem">
+          <Typography variant="h4">
+            <Elements stripe={stripePromise} options={{ clientSecret }}>
+              <PaymentStatus
+                clientSecret={clientSecret}
+                updateStatus={updatePaymentStatus}
+              />
+            </Elements>
+          </Typography>
+        </Box>
+        {isLoading ? (
+          <Loader className="small-spinner" />
+        ) : isPaymentSuccessful ? (
+          <>
+            <Box mt="2rem">
+              <Typography variant="h8">
+                {`Your order of ${totalItemsPurchased} ${
+                  totalItemsPurchased === 1 ? 'album' : 'albums'
+                } has been completed.`}
+                <br />
+                {`A confirmation email was sent to ${userEmail}`}
+              </Typography>
+            </Box>
+            <Box mt="2rem">
+              <NavigateButton
+                linkName={'/'}
+                variant={'contained'}
+                color={'secondary'}
+                children={'Return to Marketplace'}
+                fullWidth
+              />
+            </Box>
+          </>
+        ) : (
+          <Typography></Typography>
+        )}
+      </Paper>
+    </>
+  );
+};
+export default CheckoutMessageCard;

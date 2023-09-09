0463e23f039c07b25a7de5a32816faa946debed7
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-09-08 18:18:51 -0700

    The logo and icon have been added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,19 @@ import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 import Logout from './Logout';
 import { AuthStatus, useAuth } from '@akosasante/react-auth-context';
+import Logo from '../images/Logo.png';
 
 const Navbar = () => {
   const { status } = useAuth();
   const isLoggedIn = status === AuthStatus.LoggedIn;
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{ marginBottom: '2rem' }}>
       <Toolbar>
+        <img
+          src={Logo}
+          alt="Logo"
+          style={{ height: '2.5rem', marginRight: '0.625rem' }}
+        />
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Music
         </Typography>

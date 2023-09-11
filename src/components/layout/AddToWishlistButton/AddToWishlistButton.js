aedd2736ea617a1124c20ad95bc2914112df4780
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-09-11 12:29:31 -0400

    clean up `main` formatting/linting

--- src/components/layout/AddToWishlistButton/AddToWishlistButton.js
+++ src/components/layout/AddToWishlistButton/AddToWishlistButton.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Button from '@mui/material/Button';
 import axiosInstance from '../../../apis/axiosClient'; // axios instance
 import { AuthStatus, useAuth } from '@akosasante/react-auth-context';
 import { useNavigate } from 'react-router-dom';

af6d4ad5b49c0c644ab67aea79eb4f156482a2d4
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-09-19 12:01:20 -0400

    format

--- src/components/album/AlbumGrid.js
+++ src/components/album/AlbumGrid.js
@@ -6,7 +6,6 @@ import './AlbumGrid.css';
 function AlbumGrid({ albums, wishListId }) {
   return (
     <Grid container spacing={2} sx={{ marginTop: '20px' }}>
-
       {albums.map((album) => (
         <Grid item key={album._id} xs={12} sm={6} md={4} lg={3}>
           <Album album={album} wishListId={wishListId} />

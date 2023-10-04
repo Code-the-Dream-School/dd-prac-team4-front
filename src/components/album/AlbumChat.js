21a2bd076869fe8628e5a20ee89ef9d15fe2909e
Marina Tilton <95896159+mvstoyan@users.noreply.github.com>
2023-10-04 17:15:21 -0400

    PS185-send-chat-messages (#51)
    * PS185-send-chat-messages
    
    * AlbumPAge fixed
    
    * add userId
    
    * fixed small bugs

--- src/components/album/AlbumChat.js
+++ src/components/album/AlbumChat.js
@@ -1,40 +1,66 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
+import { useAuth } from '@akosasante/react-auth-context';
 
 const AlbumChat = ({ apiUrl }) => {
+  const [message, setMessage] = useState('');
+  const [socket, setSocket] = useState(null);
+  const regexPattern = /\/albums\/([a-zA-Z0-9]+)/;
+  const match = apiUrl.match(regexPattern);
+  const albumId = match ? match[1] : null;
+
+  const { user: loggedInUser } = useAuth();
+  const userId = loggedInUser?.user?.id;
+
+  // Handle sending a chat message
+  const handleSendMessage = () => {
+    if (socket && albumId) {
+      const messageData = {
+        message,
+        albumId,
+        userId,
+      };
+
+      // Emit a chat message to the 'chat:album' channel
+      socket.emit('chat:album', messageData);
+      setMessage(''); // Clear the text input field after sending
+    }
+  };
+
   useEffect(() => {
-    const socket = io(process.env.REACT_APP_SOCKET_BASE_PATH);
-
-    // Subscribe to the album chat channel using albumId
-    const regexPattern = /\/albums\/([a-zA-Z0-9]+)/;
-    const match = apiUrl.match(regexPattern);
-    if (match) {
-      // Extract albumId from the URL
-      const albumId = match[1];
-      // Сonnection is established with the Socket.io server
-      socket.on('connect', () => {
-        // Request to the server to join the album chat
+    if (albumId && !socket) {
+      const newSocket = io(process.env.REACT_APP_SOCKET_BASE_PATH);
+      newSocket.on('connect', () => {
         console.log('Connected to WebSocket server');
-        socket.emit('join:album_chat', albumId);
-
-        // Subscribe to the 'chat:album' channel
-        socket.on('chat:album', (data) => {
+        // Request to join the album chat room
+        newSocket.emit('join:album_chat', albumId);
+        newSocket.on('chat:album', (data) => {
           console.log('Received message from chat:album:', data);
         });
       });
+      setSocket(newSocket);
     }
-
     // Disconnect from the socket when the component unmounts
     return () => {
-      // Disconnect from the server
-      console.log('Disconnected from WebSocket server');
-      socket.disconnect();
+      if (socket) {
+        console.log('Disconnected from WebSocket server');
+        socket.disconnect();
+      }
     };
-  }, [apiUrl]);
+  }, [albumId, socket]);
 
   return (
     <div>
       <h2>Album Chat</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
     </div>
   );
 };

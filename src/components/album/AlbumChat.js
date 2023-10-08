2c89def1fecc4e3ca8f2ac448c1c4f3542907a15
Marina Tilton <m.v.stoyan@gmail.com>
2023-10-07 17:02:38 -0700

    style for album chat

--- src/components/album/AlbumChat.js
+++ src/components/album/AlbumChat.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import io from 'socket.io-client';
 import { useAuth } from '@akosasante/react-auth-context';
+import { Typography, TextField, Button } from '@mui/material';
 
 const AlbumChat = ({ apiUrl }) => {
   const [message, setMessage] = useState('');
@@ -13,6 +14,7 @@ const AlbumChat = ({ apiUrl }) => {
   const userId = loggedInUser?.user?.id;
 
   const [messages, setMessages] = useState([]);
+  const messagesContainerRef = useRef(null); 
 
   // Handle sending a chat message
   const handleSendMessage = () => {
@@ -21,11 +23,25 @@ const AlbumChat = ({ apiUrl }) => {
         message,
         albumId,
         userId,
+        isOwnMessage: true,
       };
 
       // Emit a chat message to the 'chat:album' channel
       socket.emit('chat:album', messageData);
       setMessage(''); // Clear the text input field after sending
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault(); 
+      handleSendMessage();
+    }
+  };
+
+  const scrollToBottom = () => {
+    if (messagesContainerRef.current) {
+      messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
     }
   };
 
@@ -41,6 +57,8 @@ const AlbumChat = ({ apiUrl }) => {
 
           // new message
           setMessages((prevMessages) => [...prevMessages, data]);
+
+          scrollToBottom();
         });
       });
       setSocket(newSocket);
@@ -54,26 +72,62 @@ const AlbumChat = ({ apiUrl }) => {
     };
   }, [albumId, socket]);
 
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
-    <div>
-      <h2>Album Chat</h2>
-      <div>
-        <div>
-          {messages.map((message, index) => (
-            <div key={index}>
-              {message.userName}: {message.message}
-            </div>
-          ))}
+    <div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column' }}>
+      <Typography variant="h6" component="div" style={{ marginLeft: '20px' }}>
+        Album Chat
+      </Typography>
+    <div style={{ flex: 1, padding: '16px', width: '95%', maxHeight: '65vh', overflowY: 'auto' }} ref={messagesContainerRef}>
+      
+      {messages.map((message, index) => (
+        <div
+          key={index}
+          style={{
+            display: 'flex',
+            flexDirection: message.isOwnMessage ? 'row-reverse' : 'row',
+            alignItems: 'center',
+            margin: '20px',
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: message.isOwnMessage ? '#3f51b5' : 'lightblue',
+              color: message.isOwnMessage ? 'white' : 'black',
+              borderRadius: '8px',
+              padding: '8px 16px',
+            }}
+          >
+            {message.message}
+          </div>
         </div>
-        <input
-          type="text"
-          placeholder="Enter message"
+      ))}
+    </div>
+    <div style={{ padding: '30px', width: '70%', display: 'flex', alignItems: 'center', position: 'fixed', bottom: 0 }}>
+      <div style={{ flex: 1 }}>
+        <TextField
+          fullWidth
+          variant="outlined"
+          label="Enter message"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={handleKeyPress}
         />
-        <button onClick={handleSendMessage}>Send</button>
+      </div>
+      <div style={{ marginLeft: '8px' }}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleSendMessage}
+        >
+          Send
+        </Button>
       </div>
     </div>
+  </div>
   );
 };
 

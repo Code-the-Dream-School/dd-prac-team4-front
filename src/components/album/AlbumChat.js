87b8c28d8ea1a1de8e7a08989ac85f5a0fd61bce
Marina Tilton <95896159+mvstoyan@users.noreply.github.com>
2023-10-14 14:37:59 -0700

    frontend for PS188 (#52)
    * frontend for PS188
    
    * style for album chat
    
    * albumChat fixed
    
    * albumChat fixed
    
    * uuid install
    
    * add key to the message
    
    * format fixed

--- src/components/album/AlbumChat.js
+++ src/components/album/AlbumChat.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import io from 'socket.io-client';
 import { useAuth } from '@akosasante/react-auth-context';
+import { Typography, TextField, Button } from '@mui/material';
+import { v4 as uuidv4 } from 'uuid';
 
 const AlbumChat = ({ apiUrl }) => {
   const [message, setMessage] = useState('');
@@ -10,7 +12,10 @@ const AlbumChat = ({ apiUrl }) => {
   const albumId = match ? match[1] : null;
 
   const { user: loggedInUser } = useAuth();
-  const userId = loggedInUser?.user?.id;
+  const loggedInUserId = loggedInUser?.user?.id;
+
+  const [messages, setMessages] = useState([]);
+  const messagesContainerRef = useRef(null);
 
   // Handle sending a chat message
   const handleSendMessage = () => {
@@ -18,12 +23,26 @@ const AlbumChat = ({ apiUrl }) => {
       const messageData = {
         message,
         albumId,
-        userId,
+        user: loggedInUserId,
       };
 
       // Emit a chat message to the 'chat:album' channel
       socket.emit('chat:album', messageData);
       setMessage(''); // Clear the text input field after sending
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      handleSendMessage();
+    }
+  };
+
+  const scrollToBottom = () => {
+    if (messagesContainerRef.current) {
+      messagesContainerRef.current.scrollTop =
+        messagesContainerRef.current.scrollHeight;
     }
   };
 
@@ -36,6 +55,16 @@ const AlbumChat = ({ apiUrl }) => {
         newSocket.emit('join:album_chat', albumId);
         newSocket.on('chat:album', (data) => {
           console.log('Received message from chat:album:', data);
+
+          const isOwnMessage = loggedInUserId === data.user._id; // Determine isOwnMessage
+          const messageWithKey = {
+            ...data,
+            isOwnMessage,
+            messageKey: uuidv4(),
+          }; // will add key to the message
+          setMessages((prevMessages) => [...prevMessages, messageWithKey]);
+
+          scrollToBottom();
         });
       });
       setSocket(newSocket);
@@ -47,19 +76,89 @@ const AlbumChat = ({ apiUrl }) => {
         socket.disconnect();
       }
     };
-  }, [albumId, socket]);
+  }, [albumId, socket, loggedInUserId]);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   return (
-    <div>
-      <h2>Album Chat</h2>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button onClick={handleSendMessage}>Send</button>
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Typography variant="h6" component="div" style={{ marginLeft: '20px' }}>
+        Album Chat
+      </Typography>
+      <div
+        style={{
+          flex: 1,
+          padding: '16px',
+          width: '95%',
+          maxHeight: '65vh',
+          overflowY: 'auto',
+        }}
+        ref={messagesContainerRef}
+      >
+        {messages.map((message) => (
+          <div key={message.messageKey}>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: message.isOwnMessage ? 'row-reverse' : 'row',
+                alignItems: 'center',
+                margin: '20px',
+              }}
+            >
+              <div
+                style={{
+                  backgroundColor: message.isOwnMessage
+                    ? '#3f51b5'
+                    : 'lightblue',
+                  color: message.isOwnMessage ? 'white' : 'black',
+                  borderRadius: '8px',
+                  padding: '8px 16px',
+                }}
+              >
+                <strong>{message.user.name}</strong>: {message.message}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div
+        style={{
+          padding: '30px',
+          width: '70%',
+          display: 'flex',
+          alignItems: 'center',
+          position: 'fixed',
+          bottom: 0,
+        }}
+      >
+        <div style={{ flex: 1 }}>
+          <TextField
+            fullWidth
+            variant="outlined"
+            label="Enter message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyPress={handleKeyPress}
+          />
+        </div>
+        <div style={{ marginLeft: '8px' }}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSendMessage}
+          >
+            Send
+          </Button>
+        </div>
       </div>
     </div>
   );

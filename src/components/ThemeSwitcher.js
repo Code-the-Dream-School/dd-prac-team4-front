377a9f9e8b1ac84308c2126ec6facbf8ebd105ae
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-11-12 16:41:22 +0100

    Edited ThemeSwitcher.js, npm run format

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -1,29 +1,43 @@
 import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
-import { useTheme } from '@mui/material/styles';
+import { useTheme, createTheme, ThemeProvider } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 function ThemeSwitcher({ toggleDarkMode }) {
   const theme = useTheme();
 
+  const modifiedDarkTheme = createTheme(theme, {
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#2196f3',
+      },
+      secondary: {
+        main: '#ff4081',
+      },
+    },
+  });
+
   return (
-    <Box>
-      <IconButton
-        sx={{ ml: 1 }}
-        onClick={() => {
-          toggleDarkMode(theme.palette.mode === 'light' ? 'dark' : 'light');
-        }}
-        color="inherit"
-      >
-        {theme.palette.mode === 'dark' ? (
-          <Brightness7Icon />
-        ) : (
-          <Brightness4Icon />
-        )}
-      </IconButton>
-    </Box>
+    <ThemeProvider theme={modifiedDarkTheme}>
+      <Box>
+        <IconButton
+          sx={{ ml: 1 }}
+          onClick={() => {
+            toggleDarkMode(theme.palette.mode === 'light' ? 'dark' : 'light');
+          }}
+          color="inherit"
+        >
+          {theme.palette.mode === 'dark' ? (
+            <Brightness7Icon />
+          ) : (
+            <Brightness4Icon />
+          )}
+        </IconButton>
+      </Box>
+    </ThemeProvider>
   );
 }
 

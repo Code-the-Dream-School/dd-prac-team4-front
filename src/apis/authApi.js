483d19688982bf1d381716d77c3731e520d4e320
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-11-15 13:16:40 +0100

    try catch block removed from authApi.js

--- src/apis/authApi.js
+++ src/apis/authApi.js
@@ -1,13 +1,9 @@
 import axios from './axiosClient';
 
 export const resetPassword = async ({ token, password }) => {
-  try {
-    const response = await axios.post('/api/v1/auth/resetPassword', {
-      token,
-      password,
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axios.post('/api/v1/auth/resetPassword', {
+    token,
+    password,
+  });
+  return response.data;
 };

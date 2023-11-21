3676f7a3ef5cc36ad31c1b9d1498cd46dbd188d3
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-11-21 15:06:53 +0100

    axios instance applied in RecommendationPage.js

--- src/components/RecommendationPage.js
+++ src/components/RecommendationPage.js
@@ -1,25 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import axiosInstance from '../api/axiosClient';
 
 const RecommendationPage = ({ userId }) => {
   const [recommendations, setRecommendations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(`/recommendations/${userId}`)
-      .then((response) => response.json())
-      .then((data) => setRecommendations(data.recommendations))
-      .catch((error) =>
-        console.error('Error fetching recommendations:', error)
-      );
+    if (userId) {
+      axiosInstance
+        .get(`/recommendations/${userId}`)
+        .then((response) => {
+          setRecommendations(response.data.recommendations);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error('Error fetching recommendations:', error);
+          setError('An error occurred while fetching recommendations.');
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
+    }
   }, [userId]);
 
   return (
     <div>
       <h1>Recommendations</h1>
-      <ul>
-        {recommendations.map((album, index) => (
-          <li key={index}>{album.albumName}</li>
-        ))}
-      </ul>
+      {error ? (
+        <p>{error}</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {recommendations.map((album) => (
+            <li key={album.albumName}>{album.albumName}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };

38ac8a35bec9a93de1e88c124292bba014b63bdc
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-12-06 16:40:07 -0800

    Changes made according to the latest mentor's recommendations

--- src/components/album/AlbumsList.js
+++ src/components/album/AlbumsList.js
@@ -24,6 +24,7 @@ import {
 
 const AlbumsList = () => {
   const [albums, setAlbums] = useState([]); // for albums
+  const [totalPages, setTotalPages] = useState(1); // Track total pages for this search
   const limit = 12;
   // const [limit, setLimit] = useState(12); // for pagination
   const [searchType, setSearchType] = useState('albumName'); //default value for select input
@@ -38,7 +39,10 @@ const AlbumsList = () => {
   const fetchAlbums = useCallback(
     async (searchType, searchTerm) => {
       try {
+        //const offset = (currentPage - 1) * limit;
         const offset = (searchTerm === '' ? currentPage - 1 : 0) * limit;
+
+        //for testing
         // console.log(currentPage - 1 + ' * ' + limit + ' = ' + offset);
         // console.log('searchType:' + searchType + ' ; searchTerm:' + searchTerm);
 
@@ -54,6 +58,7 @@ const AlbumsList = () => {
         );
         const searchResult = response.data.albums;
         setAlbums(searchResult);
+        setTotalPages(response.data.totalPages);
         if (searchResult.length === 0) {
           setMessage('No result found');
         }
@@ -66,9 +71,13 @@ const AlbumsList = () => {
     [currentPage, limit]
   );
   //display 12 albums when first user visit this page
+  // useEffect(() => {
+  //   fetchAlbums('albumName', '');
+  // }, [fetchAlbums, currentPage]); // Refetch albums when the page changes
+
   useEffect(() => {
-    fetchAlbums('albumName', '');
-  }, [fetchAlbums, currentPage]); // Refetch albums when the page changes
+    fetchAlbums(searchType, searchTerm);
+  }, [fetchAlbums, currentPage, searchTerm, searchType]);
 
   //fetch wishlist album from API
   useEffect(() => {
@@ -96,7 +105,14 @@ const AlbumsList = () => {
 
   //call the function to make API request for search input value
   const handleSearch = () => {
-    fetchAlbums(searchType, searchTerm);
+    //   fetchAlbums(searchType, searchTerm);
+
+    if (currentPage === 1) {
+      // resetting the current page to 1 won't trigger useEffect so here we have to call the fetch callback manually
+      fetchAlbums(searchType, searchTerm);
+    } else {
+      setCurrentPage(1);
+    }
   };
 
   //clear search input and make an empty API call
@@ -104,7 +120,8 @@ const AlbumsList = () => {
     //setSearchType('albumName');
     setSearchTerm('');
     setMessage('');
-    fetchAlbums('albumName', '');
+    setCurrentPage(1);
+    // fetchAlbums('albumName', '');
   };
 
   // snackbar start
@@ -197,7 +214,7 @@ const AlbumsList = () => {
               >
                 <Stack spacing={2}>
                   <Pagination
-                    count={6} //{albums.totalPages}
+                    count={totalPages} //6
                     page={currentPage}
                     color="primary"
                     onChange={handlePageChange}

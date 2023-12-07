fa4dc0253f13e44d68b7918d88585cb80f6146d1
Natacha Key <nataliaklueva@hotmail.com>
2023-12-07 11:01:08 +0100

    useCallback imported from react

--- src/components/review/ReviewsList.js
+++ src/components/review/ReviewsList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import axiosInstance from '../../apis/axiosClient';
 import WriteReview from './WriteReview';
 import { Alert } from '@mui/material';

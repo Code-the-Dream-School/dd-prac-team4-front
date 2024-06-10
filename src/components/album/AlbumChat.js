56f712da30f3aacc2e685c82daee1b68f2f558d6
Marina Tilton <m.v.stoyan@gmail.com>
2024-06-10 10:25:37 -0400

    PS165 -- AlbumPreview and AlbumChat current changes

--- src/components/album/AlbumChat.js
+++ src/components/album/AlbumChat.js
@@ -1,6 +1,6 @@
+import { useParams } from 'react-router-dom';
 import AlbumPreview from './AlbumPreview';
 import AlbumChatBox from './AlbumChatBox';
-import { useParams } from 'react-router-dom';
 
 const AlbumChat = ({ spotifyUrl }) => (
   <AlbumPreview spotifyUrl={spotifyUrl} wide contentLayout="row">
